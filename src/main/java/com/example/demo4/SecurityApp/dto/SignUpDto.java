547376d7685bcb9dd9788a2aa64e793715de90ad
AKshay Mausam <akshayjha560@gmail.com>
2024-11-18 21:06:09 +0530

    week 6 oauth2 implemented

---
+++ src/main/java/com/example/demo4/SecurityApp/dto/SignUpDto.java
@@ -0,0 +1,13 @@
+package com.example.demo4.SecurityApp.dto;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class SignUpDto {
+    private String email;
+    private String password;
+    private String name;
+
+}
